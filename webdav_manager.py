db6e7039294837b7a3470be7a54c7b3ed04254b2
wenchy <921022356@qq.com>
2025-11-22 18:09:48 +0800

    feat(完成对twitter视频下载开发): 抽取配置完成

--- webdav_manager.py
+++ webdav_manager.py
@@ -42,31 +42,6 @@ if __name__ == "__main__":
     webdav_manager = WebdavManager(config=config)
 
     # 列出目录内容
-    files = webdav_manager.ls('/sata11-13239937386/oss')
+    files = webdav_manager.ls('/')
     for file in files:
         print(f"Name: {file.name}, Type: {'Directory' if file.name.endswith('/') else 'File'}, Size: {file.size}")
-    # if webdav_client.exists('/sata11-13239937386/oss/dic/'):
-    #     print(f'directory /sata11-13239937386/oss/dic/ already exist: {len(files)}')
-
-    # webdav_client.download('/sata11-13239937386/oss/images.jpeg', 'locakimage.jpeg')
-
-    # webdav_client.cd('/sata11-13239937386/oss/dic/')
-    # res = webdav_client.upload('twitter/download/1985249127253573633xg_NJiXDU-EU-DUq.jpg', '/sata11-13239937386/oss/twitter/media/1985249127253573633xg_NJiXDU-EU-DUq.jpg')
-    #
-    # print(res)
-
-    # try:
-    #     res = webdav_client.rmdir('/sata11-13239937386/oss/dic/images.jpeg')
-    #     print(res)
-    # except OperationFailed:
-    #     print("OperationFailed")
-
-
-    # # 创建目录
-    # webdav_client.mkdir('/new_directory')
-    #
-    # # 创建嵌套目录
-    # webdav_client.mkdirs('/path/to/nested/directory')
-    #
-    # # 删除目录
-    # webdav_client.rmdir('/empty_directory')
